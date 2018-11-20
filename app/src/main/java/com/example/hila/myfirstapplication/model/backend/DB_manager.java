28ca20072f1a547f8684a2131b94b7c04965a387
hilaShamay <hilashamay4@gmail.com>
2018-11-20 11:35:13 +0200

    version 2

---
+++ app/src/main/java/com/example/hila/myfirstapplication/model/backend/DB_manager.java
@@ -0,0 +1,8 @@
+package com.example.hila.myfirstapplication.model.backend;
+import com.example.hila.myfirstapplication.model.entities.Drive;
+import java.util.List;
+
+public interface DB_manager {
+    void addDrive(Drive drive1);
+  //  List<Drive> getDrives();
+}
