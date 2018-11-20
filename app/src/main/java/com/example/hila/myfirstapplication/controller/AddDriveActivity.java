6f8fb58d46d55865511e4620b3d4c4a5b73cbb90
hilaShamay <hilashamay4@gmail.com>
2018-11-20 12:36:20 +0200

    version 3

--- app/src/main/java/com/example/hila/myfirstapplication/controller/AddDriveActivity.java
+++ app/src/main/java/com/example/hila/myfirstapplication/controller/AddDriveActivity.java
@@ -3,15 +3,11 @@ package com.example.hila.myfirstapplication.controller;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.example.hila.myfirstapplication.R;
-
-public class MainActivity extends Activity {
+public class AddDriveActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_add_drive);
     }
 }
