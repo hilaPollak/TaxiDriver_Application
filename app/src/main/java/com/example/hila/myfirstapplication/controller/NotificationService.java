a013a9991da0c06d5f46241f902f7781c391010b
hilaShamay <hilashamay4@gmail.com>
2019-01-26 22:16:09 +0200

    app completed

--- app/src/main/java/com/example/hila/myfirstapplication/controller/NotificationService.java
+++ app/src/main/java/com/example/hila/myfirstapplication/controller/NotificationService.java
@@ -12,22 +12,30 @@ import com.example.hila.myfirstapplication.model.entities.Drive;
 
 import java.util.List;
 
+/***
+ * this class represent the service that turn on when the data in drive list change and child add
+ * intent that run in background not deepened in activity
+ * send to broadcast a flag every time we had a new drive
+ */
 public class NotificationService extends Service {
 
     private int lastCount = 0;
     Context context;
     Firebase_DBManager dbManager;
 
+    /***
+     * this class said what happen each time (and not once)  a client requests something from service
+     */
     @Override
-    public int onStartCommand(final Intent intent, int flags, int startId) {
-        dbManager = (Firebase_DBManager) FactoryDataBase.getDataBase();
-        context = getApplicationContext();
-        dbManager.notifyToDriveList(new Firebase_DBManager.NotifyDataChange<List<Drive>>() {
+    public int onStartCommand(final Intent intent, int flags, int startId) {//when we turn up the service
+        dbManager = (Firebase_DBManager) FactoryDataBase.getDataBase();//set dbmanager
+        context = getApplicationContext();//get context
+        dbManager.notifyToDriveList(new Firebase_DBManager.NotifyDataChange<List<Drive>>() {//open listing to drive list
             @Override
-            public void onDataChange(List<Drive> obj) {
+            public void onDataChange(List<Drive> obj) {//add drive in list
                 try {
-                    Intent intent = new Intent(context, ToNotificationReceiver.class);
-                    sendBroadcast(intent);
+                    Intent intent = new Intent(context, ToNotificationReceiver.class);//create new intent
+                    sendBroadcast(intent);//send to broadcast the notification
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -37,9 +45,12 @@ public class NotificationService extends Service {
             public void onFailure(Exception exception) {
             }
         });
-        return START_REDELIVER_INTENT;
+        return START_REDELIVER_INTENT;//said to system to continue the service even when the application close.
     }
 
+    /***
+     * the system calls this method to retrieve the IBinder only when the first client binds(connected)
+     */
     @Override
     public IBinder onBind(Intent intent) {
         return null;
--- app/src/main/java/com/example/hila/myfirstapplication/controller/ToNotificationReceiver.java
+++ app/src/main/java/com/example/hila/myfirstapplication/controller/ToNotificationReceiver.java
@@ -14,19 +14,28 @@ import android.support.v4.app.NotificationCompat;
 
 import com.example.hila.myfirstapplication.R;
 
+/***
+ * this class said what happen each time she get something from service
+ * this class listing to service to send notification to toolbar
+ */
 public class ToNotificationReceiver extends BroadcastReceiver {
+    /***
+     * this class show how to act when he get flag from broadcast
+     * @param context the context of application
+     */
     @Override
     public void onReceive(Context context, Intent intent) {
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
-                new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+                new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);//when drive add the broadcast update
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
 
+        //ceeate notification setting
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             @SuppressLint("WrongConstant") NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_MAX);
 
-            // Configure the notification channel.
+            //setting the notification channel.
             notificationChannel.setDescription("Channel description");
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.CYAN);
